1ac0a4a90b25f4a8acdd937954708790c31bb5fa
Happiness Maina <mainahappiness361@gmail.com>
2024-12-16 20:16:11 +0300

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+ 
+  taskForm.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+    
+    const taskInput = document.getElementById("new-task-description");
+    const priority = document.getElementById("priority").value;
+    const taskText = taskInput.value.trim();
+
+    if (taskText) {
+      addTaskToDOM(taskText, priority);
+      taskInput.value = "";}
+  });
+
+  
+  function addTaskToDOM(task, priority) {
+    const taskItem = document.createElement("li");
+    taskItem.textContent = task;
+
+ 
+    if (priority === "high") taskItem.style.color = "red";
+    else if (priority === "medium") taskItem.style.color = "orange";
+    else taskItem.style.color = "green";
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "X";
+    deleteButton.style.marginLeft = "10px";
+    deleteButton.addEventListener("click", () => taskItem.remove());
+
+    
+    taskItem.appendChild(deleteButton);
+
+  
+    taskList.appendChild(taskItem);
+  }
 });
+
